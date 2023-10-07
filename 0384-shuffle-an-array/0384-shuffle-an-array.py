2b5fed6f5d88754b1114f7c680354e78ea20ff61
TaekMinKim000406 <66034252+TaekMinKim000406@users.noreply.github.com>
2023-10-08 07:17:47 +0900

    Time: 106 ms (79.37%), Space: 16.9 MB (96.3%) - LeetHub

---
+++ 0384-shuffle-an-array/0384-shuffle-an-array.py
@@ -0,0 +1,29 @@
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.original = nums[:]
+        self.nums = nums[:]
+       
+
+    def reset(self):
+        """
+        :rtype: List[int]
+        """
+        self.nums = self.original[:]
+        return self.nums    
+
+    def shuffle(self):
+        """
+        :rtype: List[int]
+        """
+        random.shuffle(self.nums)
+        return self.nums 
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
